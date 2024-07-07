a589f68eb02ab3c548c6db71726ee2fdd71c6563
gvandevi <gabrielle.vandevijver@gmail.com>
2024-07-08 01:17:47 +0200

    files cleaning

--- helpers.py
+++ helpers.py
@@ -3,7 +3,82 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import pickle
 import os
+import pandas as pd
 
+#Data preprocessing
+def label_text(csv_path):
+    label_list = []
+    r = pd.read_csv(csv_path)
+    # Going through all names
+    for name in r['SignName']:
+        label_list.append(name)
+    # Returning resulted list with labels
+    return label_list
+
+def unprocess_record(source_record):
+    low, high = np.min(source_record), np.max(source_record)
+    image_array = 255.0 * (source_record - low) / (high - low)
+    return image_array
+
+def reprocess(array,source_record):
+    low, high = np.min(source_record), np.max(source_record)
+    reprocessed_record = array * ((high - low) / 255.0) + low
+    return reprocessed_record 
+
+def save_data_pickle(data, file_path):
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f)
+
+def load_pickle(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+    
+def load_data_from_pickle(index,base_path):
+    # Define the file paths based on provided names
+    train_data_path = base_path+'x_train'+str(index)+'.pickle'
+    validation_data_path = base_path+'x_val'+str(index)+'.pickle'
+    test_data_path = base_path+'x_test'+str(index)+'.pickle'
+
+    train_label_path = base_path+'y_train'+str(index)+'.pickle'
+    validation_label_path = base_path+'y_val'+str(index)+'.pickle'
+    test_label_path = base_path+'y_test'+str(index)+'.pickle'
+
+    # Load datasets
+    x_train = load_pickle(train_data_path)
+    x_val = load_pickle(validation_data_path)
+    x_test = load_pickle(test_data_path)
+
+    # Load labels
+    y_train = load_pickle(train_label_path)
+    y_val = load_pickle(validation_label_path)
+    y_test = load_pickle(test_label_path)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+#Images processing
+
+def print_image(image_array,title):
+    fig = plt.figure()
+    plt.imshow(image_array.astype('uint8'), cmap='gray')
+    plt.axis('off')
+    plt.gcf().set_size_inches(2, 2)
+    plt.title(title)
+    plt.show()
+    plt.close()
+    return
+
+def downscale(image_array,ratio):
+    image = Image.fromarray(np.uint8(image_array))
+    new_size = (image.width // ratio, image.height // ratio) 
+    downscaled_image = image.resize(new_size, Image.Resampling.LANCZOS)
+    downscaled_image_array = np.array(downscaled_image)
+    return downscaled_image_array
+
+def upscale_image(image_array,n):
+    image = Image.fromarray(np.uint8(image_array))
+    upscaled_image = image.resize((n,n), Image.Resampling.LANCZOS)
+    upscaled_array = np.array(upscaled_image)
+    return upscaled_array
 
 def convert_to_grid(x_input):
     """
@@ -34,42 +109,11 @@ def convert_to_grid(x_input):
 
     return grid
 
-
-def save_data_pickle(data, file_path):
-    with open(file_path, 'wb') as f:
-        pickle.dump(data, f)
-
-def load_pickle(file_path):
-    with open(file_path, 'rb') as f:
-        return pickle.load(f)
-    
-def load_data_from_pickle(index):
-    # Define the file paths based on provided names
-    train_data_path = '../SE4AI_data/x_train'+str(index)+'.pickle'
-    validation_data_path = '../SE4AI_data/x_val'+str(index)+'.pickle'
-    test_data_path = '../SE4AI_data/x_test'+str(index)+'.pickle'
-
-    train_label_path = '../SE4AI_data/y_train'+str(index)+'.pickle'
-    validation_label_path = '../SE4AI_data/y_val'+str(index)+'.pickle'
-    test_label_path = '../SE4AI_data/y_test'+str(index)+'.pickle'
-
-    # Load datasets
-    x_train = load_pickle(train_data_path)
-    x_val = load_pickle(validation_data_path)
-    x_test = load_pickle(test_data_path)
-
-    # Load labels
-    y_train = load_pickle(train_label_path)
-    y_val = load_pickle(validation_label_path)
-    y_test = load_pickle(test_label_path)
-
-    return x_train, y_train, x_val, y_val, x_test, y_test
-
-
 def apply_trigger(image, mask, trigger):
     return tf.where(mask == 1, trigger, image)
 
 
+#Model retraining
 def construct_balanced_dataset_variable_size(x_train, y_train, target_class, source_class, trigger, mask, p=1.0):
     """
     :param x_train:
